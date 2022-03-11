096417a814809e4e3c90f3a83b66cfa4691c2e03
TheDarkSword <TheDarkSword01@gmail.com>
2022-03-11 23:49:37 +0100

    Implemented listener

--- src/main/java/it/thedarksword/skillmastery/SkillMastery.java
+++ src/main/java/it/thedarksword/skillmastery/SkillMastery.java
@@ -3,50 +3,84 @@ package it.thedarksword.skillmastery;
 import ch.jalu.configme.SettingsManager;
 import ch.jalu.configme.SettingsManagerBuilder;
 import it.thedarksword.basement.api.bukkit.BasementBukkit;
+import it.thedarksword.skillmastery.blocks.BlockManager;
 import it.thedarksword.skillmastery.config.SkillConfig;
+import it.thedarksword.skillmastery.listeners.PlayerListener;
 import it.thedarksword.skillmastery.mysql.manager.QueryManager;
 import it.thedarksword.skillmastery.player.manager.PlayerManager;
+import it.thedarksword.skillmastery.potions.PotionManager;
 import it.thedarksword.skillmastery.skill.provider.SkillProvider;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+import org.bukkit.block.Block;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
 @Accessors(fluent = true)
 public class SkillMastery {
 
+    @Getter
+    @Accessors(fluent = true)
+    private static SkillMastery instance;
+
     private final JavaPlugin plugin;
     private final BasementBukkit basement;
+    private final CoreProtectAPI coreProtectAPI;
 
     private final SettingsManager config;
-    private final QueryManager queryManager;
 
     private final SkillProvider skillProvider;
+    private final QueryManager queryManager;
     private final PlayerManager playerManager;
-
+    private final BlockManager blockManager;
+    private final PotionManager potionManager;
 
     public SkillMastery(JavaPlugin plugin) {
+        instance = this;
         this.plugin = plugin;
         this.basement = Objects.requireNonNull(plugin.getServer().getServicesManager().getRegistration(BasementBukkit.class)).getProvider();
+        coreProtectAPI = getCoreProtect();
 
         this.config = SettingsManagerBuilder.withYamlFile(new File(plugin.getDataFolder(), "config.yml"))
                 .configurationData(SkillConfig.class).useDefaultMigrationService().create();
 
-        queryManager = new QueryManager(basement, config);
-
         skillProvider = new SkillProvider();
-
+        queryManager = new QueryManager(basement, config);
         playerManager = new PlayerManager();
+        blockManager = new BlockManager(coreProtectAPI);
+        potionManager = new PotionManager();
     }
 
     protected void enable() {
 
+        plugin.getServer().getPluginManager().registerEvents(new PlayerListener(this), plugin);
     }
 
     protected void disable() {
 
+    }
+
+    private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = plugin().getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that the API is enabled
+        CoreProtectAPI coreProtectAPI = ((CoreProtect) Objects.requireNonNull(plugin)).getAPI();
+        if (!coreProtectAPI.isEnabled()) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (coreProtectAPI.APIVersion() < 9) {
+            return null;
+        }
+
+        return coreProtectAPI;
     }
 }
--- src/main/java/it/thedarksword/skillmastery/listeners/PlayerListener.java
+++ src/main/java/it/thedarksword/skillmastery/listeners/PlayerListener.java
@@ -1,18 +1,34 @@
 package it.thedarksword.skillmastery.listeners;
 
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import it.thedarksword.skillmastery.SkillMastery;
 import it.thedarksword.skillmastery.player.SkillPlayer;
+import it.thedarksword.skillmastery.skill.Skill;
 import it.thedarksword.skillmastery.skill.SkillType;
 import lombok.RequiredArgsConstructor;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_18_R2.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.BrewerInventory;
 
 @RequiredArgsConstructor
-public class PlayerListener {
+public class PlayerListener implements Listener {
 
     private final SkillMastery skillMastery;
 
@@ -22,6 +38,7 @@ public class PlayerListener {
         if(skillMastery.playerManager().hasCache(player)) {
             skillMastery.playerManager().restorePlayer(player);
         } else {
+            skillMastery.queryManager().skillPlayerAdd(player.getUniqueId()).exec();
             skillMastery.playerManager().skillPlayer(player, new SkillPlayer(player, skillMastery));
         }
     }
@@ -31,6 +48,7 @@ public class PlayerListener {
         handleQuit(event.getPlayer());
     }
 
+    @EventHandler
     public void onKick(PlayerKickEvent event) {
         handleQuit(event.getPlayer());
     }
@@ -39,9 +57,75 @@ public class PlayerListener {
         skillMastery.playerManager().cachePlayer(player);
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockBreak(BlockBreakEvent event) {
+        Location location = event.getBlock().getLocation();
+        ApplicableRegionSet regions = WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(event.getBlock().getWorld()))
+                .getApplicableRegions(BlockVector3.at(location.getBlockX(), location.getBlockY(), location.getBlockZ()));
+        for (ProtectedRegion region : regions.getRegions()) {
+            if (region.getId().startsWith("miner")) {
+                Skill<BlockBreakEvent> skill = skillMastery.playerManager().skillPlayer(event.getPlayer()).skill(SkillType.MINING);
+                skill.experience();
+                skill.process(event);
+                return;
+            }
+        }
+        Material type = event.getBlock().getType();
+        if(skillMastery.blockManager().isLog(type)) {
+            Skill<BlockBreakEvent> skill = skillMastery.playerManager().skillPlayer(event.getPlayer()).skill(SkillType.FORAGING);
+            skill.experience();
+            skill.process(event);
+        } else if(skillMastery.blockManager().isFarm(type)) {
+            Skill<BlockBreakEvent> skill = skillMastery.playerManager().skillPlayer(event.getPlayer()).skill(SkillType.FARMING);
+            skill.experience();
+            skill.process(event);
+        }
+    }
+
     @EventHandler
     public void onDeath(EntityDeathEvent event) {
         if(event.getEntity().getKiller() == null) return;
-        skillMastery.playerManager().skillPlayer(event.getEntity().getKiller()).skill(SkillType.COMBAT).experience();
+        skillMastery.playerManager().skillPlayer(event.getEntity().getKiller()).<EntityDeathEvent>skill(SkillType.COMBAT).experience();
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if(!(event.getDamager() instanceof Player player) || (event.getEntity() instanceof Player)) return;
+        skillMastery.playerManager().skillPlayer(player).<EntityDamageByEntityEvent>skill(SkillType.COMBAT).process(event);
+    }
+
+    @EventHandler
+    public void onFishing(PlayerFishEvent event) {
+        if(event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+            Skill<PlayerFishEvent> skill = skillMastery.playerManager().skillPlayer(event.getPlayer()).skill(SkillType.FISHING);
+            skill.experience();
+            skill.process(event);
+        }
+    }
+
+    @EventHandler
+    public void onEnchant(EnchantItemEvent event) {
+        skillMastery.playerManager().skillPlayer(event.getEnchanter()).<EnchantItemEvent>skill(SkillType.ENCHANTING).experience();
+    }
+
+    @EventHandler
+    public void onExp(PlayerExpChangeEvent event) {
+        skillMastery.playerManager().skillPlayer(event.getPlayer()).<PlayerExpChangeEvent>skill(SkillType.ENCHANTING).process(event);
+    }
+
+    @EventHandler
+    public void onBrew(InventoryClickEvent event) {
+        if(!(event.getClickedInventory() instanceof BrewerInventory) || event.getCurrentItem() == null ||
+                event.getCurrentItem().getType() != Material.POTION) return;
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getCurrentItem());
+        NBTTagCompound compound = itemStack.t();
+        if(compound.b("taked")) return;
+
+        Skill<InventoryClickEvent> skill = skillMastery.playerManager().skillPlayer((Player) event.getWhoClicked()).skill(SkillType.ALCHEMY);
+        skill.experience();
+        skill.process(event);
+
+        compound.a("taked", true);
+        event.setCurrentItem(CraftItemStack.asBukkitCopy(itemStack));
     }
 }
